f55f4a43bf4121e399fbc31463544d880f6fed0a
Ray Alez <raymestalez@gmail.com>
2017-06-20 10:35:47 -0700

    Paypal payments.

--- client/components/ModalPayments.js
+++ client/components/ModalPayments.js
@@ -37,9 +37,7 @@ class ModalPayments extends Component {
 		    <h1>Upgrade Account</h1>
 		</Modal.Header>
 		<div className="panel-modal">
-		    <p>You are currently using a free version of Nulis,
-			which allows you to create up to {this.props.user.cardLimit} 
-			cards per month.</p>
+		    <p>You are currently using a free version of Nulis, which allows you to create up to {this.props.user.cardLimit} cards per month.</p>
 		    <p>Upgrade your account to create unlimited cards (both online and on desktop), get priority support, early access to new features, and help me to make this project even more awesome!</p>
 		    <p>Because Nulis has launched only recently, as an early adopter
 			you can purchase the unlimited lifetime account for only 
